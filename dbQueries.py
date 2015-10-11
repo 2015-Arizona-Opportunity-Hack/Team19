e1bafaf3c5d0a93600a920ce04197e95b5ab958c
Sai Pc <schand31@asu.edu>
2015-10-10 22:55:41 -0700

    Queries over

--- dbQueries.py
+++ dbQueries.py
@@ -1,4 +1,19 @@
 __author__ = 'saipc'
 import psycopg2
+conn = psycopg2.connect(database="SMC", user="postgres", password="dragon123", host="127.0.0.1", port="5432")
+cur = conn.cursor()
+table_name1 = "Orders"
+table_name2 = "productData"
+table_name3 = "wufooforms"
+query = 'select * from "Orders", "productData", "wufooforms" where "Orders"."ProductName" = "productData"."ProductName" and "wufooforms"."orderID" = "Orders"."OrderID" '
+print query
+cur.execute(query)
+rows = cur.fetchall()
+count = 0
+for row in rows:
+    #print row
+    count += 1
+print count
+conn.commit()
 
 # select * from "Orders", "productData" where "Orders"."ProductName" = "productData"."ProductName"
