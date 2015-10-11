406b0faeaee944c6d7d154ccac6a03d4bbf0b7ce
Sai Pc <schand31@asu.edu>
2015-10-11 01:27:28 -0700

    Send email script

--- generateEmail.py
+++ generateEmail.py
@@ -1,5 +1,8 @@
 __author__ = 'saipc'
 
+import smtplib
+from email.mime.text import MIMEText
+
 def generateEmail(name, message, image):
     email = """
 <!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title></title></head><body>
@@ -9,3 +12,23 @@ def generateEmail(name, message, image):
 <p>""" + message + """</p></td><td colspan='1'><img src='""" + image + """'></td></tr>
 <tr><td colspan="4"><img src="footer.jpg" width="600"></td></tr></tbody></table></div></body></html>
 """
+    return email
+
+def sendEmail(email, address):
+
+    msg = MIMEText(email, 'html')
+    me = "schand31@asu.edu"
+    them = "swetha.baskaran@asu.edu"
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'Thank You note from SupportMyClub'
+    msg['From'] = me
+    msg['To'] = them
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, [them], msg.as_string())
+    s.quit()
+
+
