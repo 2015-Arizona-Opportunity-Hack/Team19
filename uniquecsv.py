eff98fcffa0e4c78de473450f7883adce2bcbb4a
Sai Pc <schand31@asu.edu>
2015-10-10 19:37:12 -0700

    parsing done for orders

--- uniquecsv.py
+++ uniquecsv.py
@@ -2,7 +2,6 @@ __author__ = 'saipc'
 
 with open('orders_export.csv', 'r') as csv:
     orderIDs = set()
-    orders = dict()
     for line in csv:
         strings = line.split(",")
         if(len(strings) > 2):
@@ -12,6 +11,10 @@ with open('orders_export.csv', 'r') as csv:
 
         nonNames = ['pending', 'true', 'fulfilled']
         if(orderID and email and name and name not in nonNames):
-            print orderID, email, name
+            if orderID not in orderIDs:
+                orderIDs.add(orderID)
+                print (orderID + ',' + email + ',' + name)
+                with open('orders', 'a') as writer:
+                    writer.write(orderID + ',' + email + ',' + name + "\n")
             #orders[orderID] = (email, name)
 
