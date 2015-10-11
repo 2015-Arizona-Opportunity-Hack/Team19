c2123412351baf6072af5f8e3f27669adb895dd6
Sai Pc <schand31@asu.edu>
2015-10-10 19:57:27 -0700

    MOAAARRR PARSING

--- uniquecsv.py
+++ uniquecsv.py
@@ -8,13 +8,18 @@ with open('orders_export.csv', 'r') as csv:
             orderID = strings[0]
             email = strings[1]
             name = strings[24]
+            donation = strings[11]
+            productName = strings[17]
 
         nonNames = ['pending', 'true', 'fulfilled']
-        if(orderID and email and name and name not in nonNames):
+        if(orderID and email and name and name not in nonNames and donation):
             if orderID not in orderIDs:
                 orderIDs.add(orderID)
                 print (orderID + ',' + email + ',' + name)
                 with open('orders', 'a') as writer:
-                    writer.write(orderID + ',' + email + ',' + name + "\n")
+                    writer.write(orderID + ',' + email + ',' + name + "," + productName +"\n")
+                with open('recommenderData.csv', 'a') as writer:
+                    writer.write(name + "," + productName + "," + donation +"\n")
+
             #orders[orderID] = (email, name)
 
